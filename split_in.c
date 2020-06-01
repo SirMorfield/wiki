3e317d9af2c37f3daa623a8545a4d5fac93c02fd
SirMorfield <joppe.koers@gmail.com>
2020-06-01 18:27:29 +0200

    A

--- split_in.c
+++ split_in.c
@@ -1,86 +1,37 @@
-#include <fcntl.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
-#define BLOCKSIZE 6
+ssize_t split_in(char*** items, char* file_name, char seperator) {
+    FILE* infile = fopen(file_name, "r");
+    if (infile == NULL) return -1;
 
-long split_in(char*** strings, char* file_name, char seperator) {
-    int fd = open(file_name, O_RDONLY);
-    if (fd < 0) return -1;
-    size_t offset = 0;
-    size_t length = 0;
-    ssize_t bytes_read;
-    size_t words_read = 0;
-    bool reached_end = false;
-    *strings = malloc(0 * sizeof(char*));
-    char* read_buffer = malloc(0 * sizeof(char));
-    while (1) {
-        ssize_t length = -1;
-        size_t buf_size = 0;
-        size_t total_bytes_read = 0;
-        read_buffer = malloc(buf_size * sizeof(char));
-        if (read_buffer == NULL) return -1;
+    fseek(infile, 0L, SEEK_END);
+    long numbytes = ftell(infile);
+    fseek(infile, 0L, SEEK_SET);  // reset the file position indicator to the beginning of the file
+    char* buffer = malloc(numbytes * sizeof(char));
+    fread(buffer, sizeof(char), numbytes, infile);
+    fclose(infile);
 
-        while (1) {
-            buf_size += BLOCKSIZE;
-            read_buffer = realloc(read_buffer, buf_size * sizeof(char));
-            if (read_buffer == NULL) return -1;
+    size_t items_read = 0;
+    *items = malloc(0 * sizeof(char*));
+    long right_index = numbytes;
+    for (ssize_t left_index = right_index - 1; left_index >= 0; left_index--) {
+        if (buffer[left_index - 1] == seperator || left_index == 0) {
+            *items = realloc(*items, (items_read + 1) * sizeof(char*));
+            size_t string_size = right_index - left_index;
+            (*items)[items_read] = malloc((string_size + 1) * sizeof(char));
+            memcpy((*items)[items_read], buffer + left_index, string_size);
+            (*items)[items_read][string_size] = '\0';
+            items_read++;
+            right_index = left_index - 1;
 
-            bytes_read = pread(fd, read_buffer + total_bytes_read, BLOCKSIZE, offset);
-            total_bytes_read += bytes_read;
-
-            if (bytes_read < BLOCKSIZE) {
-                reached_end = true;
-                length = total_bytes_read;
-                break;
-            }
-
-            for (ssize_t i = total_bytes_read - 1; i >= (total_bytes_read - bytes_read); i--) {
-                if (read_buffer[i] == seperator) {
-                    length = i;
-                    break;
-                }
-            }
-
-            if (length != -1) {
-                offset += (total_bytes_read - length) + 1;
-                break;
-            } else {
-                offset += bytes_read;
-            }
+            buffer = realloc(buffer, (numbytes - string_size) * sizeof(char));
         }
-
-        printf("<%s>", read_buffer);
-        printf("%ld\n", length);
-        ssize_t prev_seperator_index = 0;
-        for (size_t i = 1; i < length; i++) {
-            if (read_buffer[i] == seperator || i == (length - 1)) {
-                *strings = realloc(*strings, (words_read + 1) * sizeof(char*));
-                size_t string_size = (i - prev_seperator_index) + 1;
-
-                (*strings)[words_read] = malloc((string_size + 1) * sizeof(char));
-                memcpy((*strings)[words_read], read_buffer + prev_seperator_index, string_size);
-                (*strings)[words_read][string_size] = '\0';
-                words_read++;
-                prev_seperator_index = i + 1;
-            }
-        }
-        if (reached_end) break;
     }
-    return words_read;
-}
-int main() {
-    char** strings;
-    long words_read = split_in(&strings, "data/test", ' ');
-    if (words_read > 0) {
-        printf("words_read %ld\n", words_read);
-        for (size_t i = 0; i < words_read; i++) {
-            printf("<%s>\n", strings[i]);
-        }
-    } else {
-        printf("err");
-    }
+
+    // TODO: reversing
+
+    free(buffer);
+    return items_read;
 }
